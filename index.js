f8e147abd7089de6489654b944f5a320179ba2f8
rampravesh9991 <rampravesh9991@gmail.com>
2022-07-21 07:50:03 +0530

    rampravesh homepage updated

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
 let data = [
-    {id : 1,title : "Mind maps",content : "Mind Mapping is proven to boost productivity, creativity and memory. Grow ideas, structure information, and see the whole picture.",sub : "How to use mind maps",pic : "https://www.ayoa.com/wp-content/uploads/2021/11/mm_thumbnail_new.png",image : "https://www.ayoa.com/wp-content/uploads/2021/03/Unrivaled_mm_new_webp.webp"},
-    {id : 2,title : "Whiteboards",content : "Use whiteboards for freeform thinking or to create content dashboards. Add post-it notes, images, flowcharts, links, videos and more.",sub : "How to use whiteboards",pic : "https://www.ayoa.com/wp-content/uploads/2021/11/wb_thumbnail_new.png",image : "https://www.ayoa.com/wp-content/uploads/2021/03/Unrivaled_wb_new_webp_2.webp"},
-    {id : 3,title : "Workflow",content : "Inspired by the traditional Kanban board, this visual task board view lets you manage your projects and workflows in a clear, linear way.",sub : "How to use workflow",pic : "https://www.ayoa.com/wp-content/uploads/2021/11/workflow_thumbnail_new.png",image : "https://www.ayoa.com/wp-content/uploads/2021/03/Unrivaled_kanban_new_webp.webp"},
-    {id : 4,title : "Canvas",content : "Discover a more creative way to manage your tasks. Canvas view makes it easy to visualize your work in a fun circular format.",sub : "How to use canvas",pic : "https://www.ayoa.com/wp-content/uploads/2021/11/canvas_thumbnail_new.png",image : "https://www.ayoa.com/wp-content/uploads/2021/03/Unrivaled_canvas_new_webp.webp"},
-    {id : 5,title : "Gantt timeline",content : "Working on a project with multiple milestones? View your tasks in clear and easily adaptable Gantt timeline mode.",sub : "How to use gantt timeline",pic : "https://www.ayoa.com/wp-content/uploads/2021/11/gantt_thumbnail_new.png",image : "https://www.ayoa.com/wp-content/uploads/2021/03/Unrivaled_gantt_new_webp.webp"},
-    {id : 6,title : "Radial",content : "Ready to make every goal a reality? Present your aims and ideas in a visual pie-chart and stay motivated by seeing your priorities at a glance.",sub : "How to use radial maps",pic : "https://www.ayoa.com/wp-content/uploads/2021/11/radial_thumbnail_new.png",image : "https://www.ayoa.com/wp-content/uploads/2021/03/Unrivaled_radial_new_webp.webp"},
-    {id : 7,title : "Capture maps",content : "Capture maps are the quickest and easiest way to capture notes and ideas. Great for brainstorming and note making.",sub : "How to use capture maps",pic : "https://www.ayoa.com/wp-content/uploads/2022/06/fc_video_thumbnail.png",image : "https://www.ayoa.com/wp-content/uploads/2022/06/capturemaps.webp"}
+    {
+        id : 1,
+        title : "Mind maps",
+        content : "Mind Mapping is proven to boost productivity, creativity and memory. Grow ideas, structure information, and see the whole picture.",
+        sub : "How to use mind maps",
+        pic : "https://www.ayoa.com/wp-content/uploads/2021/11/mm_thumbnail_new.png",
+        image : "https://www.ayoa.com/wp-content/uploads/2021/03/Unrivaled_mm_new_webp.webp"
+    },
+    {
+        id : 2,
+        title : "Whiteboards",
+        content : "Use whiteboards for freeform thinking or to create content dashboards. Add post-it notes, images, flowcharts, links, videos and more.",
+        sub : "How to use whiteboards",
+        pic : "https://www.ayoa.com/wp-content/uploads/2021/11/wb_thumbnail_new.png",
+        image : "https://www.ayoa.com/wp-content/uploads/2021/03/Unrivaled_wb_new_webp_2.webp"
+    },
+    {
+        id : 3,
+        title : "Workflow",
+        content : "Inspired by the traditional Kanban board, this visual task board view lets you manage your projects and workflows in a clear, linear way.",
+        sub : "How to use workflow",
+        pic : "https://www.ayoa.com/wp-content/uploads/2021/11/workflow_thumbnail_new.png",
+        image : "https://www.ayoa.com/wp-content/uploads/2021/03/Unrivaled_kanban_new_webp.webp"
+    },
+    {
+        id : 4,
+        title : "Canvas",
+        content : "Discover a more creative way to manage your tasks. Canvas view makes it easy to visualize your work in a fun circular format.",
+        sub : "How to use canvas",
+        pic : "https://www.ayoa.com/wp-content/uploads/2021/11/canvas_thumbnail_new.png",
+        image : "https://www.ayoa.com/wp-content/uploads/2021/03/Unrivaled_canvas_new_webp.webp"
+    },
+    {id : 5,
+        title : "Gantt timeline",
+        content : "Working on a project with multiple milestones? View your tasks in clear and easily adaptable Gantt timeline mode.",
+        sub : "How to use gantt timeline",
+        pic : "https://www.ayoa.com/wp-content/uploads/2021/11/gantt_thumbnail_new.png",
+        image : "https://www.ayoa.com/wp-content/uploads/2021/03/Unrivaled_gantt_new_webp.webp"
+    },
+    {
+        id : 6,
+        title : "Radial",
+        content : "Ready to make every goal a reality? Present your aims and ideas in a visual pie-chart and stay motivated by seeing your priorities at a glance.",
+        sub : "How to use radial maps",
+        pic : "https://www.ayoa.com/wp-content/uploads/2021/11/radial_thumbnail_new.png",
+        image : "https://www.ayoa.com/wp-content/uploads/2021/03/Unrivaled_radial_new_webp.webp"
+    },
+    {
+        id : 7,
+        title : "Capture maps",
+        content : "Capture maps are the quickest and easiest way to capture notes and ideas. Great for brainstorming and note making.",
+        sub : "How to use capture maps",
+        pic : "https://www.ayoa.com/wp-content/uploads/2022/06/fc_video_thumbnail.png",
+        image : "https://www.ayoa.com/wp-content/uploads/2022/06/capturemaps.webp"
+    }
 ]
 
 let x = 1;
@@ -61,7 +109,5 @@ function displayData(arr){
     container.append(card1,card2);
 
 }
-
-
 //this will work as onload- variable x will help in choosing very first data
 main();

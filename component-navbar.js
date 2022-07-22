8dde90dd2464c763c9748a4bcb954eabb21abfd5
rampravesh9991 <rampravesh9991@gmail.com>
2022-07-22 18:07:22 +0530

    rampravesh after-login-nav

---
+++ component-navbar.js
@@ -0,0 +1,31 @@
+let navbar = () =>{
+    return `<div id="navbar">
+    <div>
+      <span id="cir">
+        <i class="fa-solid fa-comment-dots fa-2xl
+        "></i>
+      </span>
+      <i class="fa-solid fa-bars fa-xl"></i>
+      <img src="https://app-assets.ayoa.com/8.90.1/images/icons/ayoa.svg" />
+    </div>
+    <div>
+      <hr/>
+    </div>
+    <div>
+      <img src="https://app-assets.ayoa.com/8.90.1/images/icons/home.svg" />
+      <img src="https://app-assets.ayoa.com/8.90.1/images/icons/pulse.svg"/>
+      <img src="https://app-assets.ayoa.com/8.90.1/images/icons/planner.svg"/>
+      <img src="https://app-assets.ayoa.com/8.90.1/images/icons/notifications-icon.png"/>
+      <img src="https://app-assets.ayoa.com/8.90.1/images/icons/search-icon.png"/>
+    </div>
+    <div>
+      <hr/>
+    </div>
+    <div></div>
+  </div>
+  <div id="trial">
+    <p>Trial version <b>6 days remaining</b></p>
+    <button>Buy Now<span></span>
+  </div>`
+} 
+export default navbar;

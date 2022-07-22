3846f997d972f1da019efb2e6ebbf444d2093c83
rampravesh9991 <rampravesh9991@gmail.com>
2022-07-22 22:47:38 +0530

    RAMPRAVESH - Landing page done

---
+++ index-import.js
@@ -0,0 +1,5 @@
+//------------------------Navbar-----------------------------------
+import {navbar_avnish} from "./component_avnish/navbar_avnish.js";
+console.log(navbar_avnish());
+
+document.getElementById("navbar").innerHTML = navbar_avnish();
